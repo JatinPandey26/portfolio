1cca595b330062be2f55c15a853e756f26166a15
Jatin pandey <jatin26pandey@gmail.com>
2023-03-20 16:42:06 +0530

    fix

--- src/Components/HomeLeftSection/HomeLeft.jsx
+++ src/Components/HomeLeftSection/HomeLeft.jsx
@@ -17,7 +17,7 @@ const HomeLeft = () => {
                 </div>
             </div>
             <p>
-                <svg width="3rem" height="2rem" xmlns="http://www.w3.org/2000/svg" fill-rule="evenodd" clip-rule="evenodd"><path d="M21.883 12l-7.527 6.235.644.765 9-7.521-9-7.479-.645.764 7.529 6.236h-21.884v1h21.883z" /></svg>    WELCOME TO HAKAJI! MEET OUR COLLECTION OF 3.333 UNIQUE GIRAFFE SLAM. SUPPORT AND JOIN OUR COMMUNITY, A NEW WORLD ARE WAITING FOR YOU.</p>
+                <svg width="3rem" height="2rem" xmlns="http://www.w3.org/2000/svg" fill-rule="evenodd" clip-rule="evenodd"><path d="M21.883 12l-7.527 6.235.644.765 9-7.521-9-7.479-.645.764 7.529 6.236h-21.884v1h21.883z" /></svg>    An innovative and diverse collection showcasing my skills, achievements, and passions with creativity and impact.</p>
 
         </div>
     )
